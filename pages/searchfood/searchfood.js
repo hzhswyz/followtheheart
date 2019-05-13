0df299723922567b9a67e80e60535c3ca4d975bc
HZH <hzhswyz@163.com>
2019-05-13 15:48:53 +0800

    xg

--- pages/searchfood/searchfood.js
+++ pages/searchfood/searchfood.js
@@ -2,8 +2,6 @@
 const app = getApp();
 var durl = app.globalData.dynamicrequest;
 var storelist = [];
-var latitude = null;
-var longitude = null;
 var pageobject;
 //当前加载的页数
 var currentpage = 1;
@@ -35,151 +33,66 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    storelist = [];
-    currentpage = 1;
-    pageobject = this;
-    keyword = options.keyword;
-    //检查用户是否授予定位权限
-    function positionpromise() {
-      var userpositionpromise = new Promise(function (resolve, reject) {
-        //获取用户位置
-        wx.getSetting({
-          success: function (res) {
-            console.log(res);
-            if (res.authSetting['scope.userLocation']) {
-              console.log("userpositionpromise 已经获取用户位置授权了");
-              resolve();
-            }
-            else {
-              reject(new Error("没有获取位置授权，将通过wx.authorize获取用户位置获取"));
-            }
-          },
-          fail: function (res) {
-            console.log("调用wx.getSetting（）失败，将通过wx.authorize获取用户位置获取");
-            reject(new Error("调用wx.getSetting（）失败"));
-          }
-        });
-      });
-      return userpositionpromise;
-    }
+  getstores:function () {
 
-    //向用户获取位置权限
-    function getlocaltionauthorizepromise() {
 
-      var authorizepromise = new Promise(function (resolve, reject) {
-        wx.authorize({
-          scope: "scope.userLocation",
-          success: function () {
-            console.log("用户同意授权位置权限");
-            resolve();
-          },
-          fail: function () {
-            console.log("用户拒绝授权位置权限");
-            reject(new Error("用户拒绝授权位置权限"));
-          }
-        });
-      });
-      return authorizepromise;
-    }
+    var localinfo = {};
+    localinfo.la = app.globalData.latitude;
+    localinfo.lo = app.globalData.longitude;
 
-    //获取用户位置信息（经纬度信息）
-    function getlocationpromise() {
 
-      var longitudeandlatitude = new Promise(function (resolve, reject) {
-        wx.getLocation({
-          success: function (res) {
-            console.log("经度" + res.latitude);
-            console.log("纬度" + res.longitude);
-            latitude = res.latitude;
-            longitude = res.longitude;
-            resolve({ la: latitude, lo: longitude });
-          },
-          fail: function (res) {
-            console.log("getLocationpromise", res);
-            reject(new Error("wx.getLocation获取用户位置失败"));
-          }
-        });
-      });
-      return longitudeandlatitude;
-    }
+    var data = { latitude: localinfo.la, longitude: localinfo.lo, currentpage: currentpage, keyword: keyword };
 
-    function getstorespromise(localinfo) {
-
-      //没有执行getlocationpromise（）方法情况下
-      if (localinfo == undefined) {
-        localinfo = {};
-        localinfo.la = latitude;
-        localinfo.lo = longitude;
-      }
-
-      var data = { latitude: localinfo.la, longitude: localinfo.lo, currentpage: currentpage, keyword: keyword };
-
-      console.log("data", data)
+    console.log("data", data)
 
       wx.request({
-        url: durl + "/store/searchfood",
-        header: { Cookie: "JSESSIONID=" + app.globalData.session },
-        data: data,
-        success: function (res) {
-          if (res.data.status == 200) {
-            console.log("相关商店", res.data.data)
-            var list = res.data.data;
-            wx.hideLoading();
-            if (list.length == 0) {
-              wx.showToast({
-                icon: "none",
-                title: "没有更多数据了",
-              })
-              return;
-            }
-            for (var i = 0; i < list.length; i++) {
-              list[i].type = list[i].type.split(",");
-              list[i].imgsrc = durl + "/static/image/" + list[i].id + "image.jpg";
-              for (var z = 0; z < list[i].foodlist.length; z++)
-                list[i].foodlist[z].foodimg = durl + "/static/image/food/food" + list[i].foodlist[z].foodid + ".jpg";
-            }
-            storelist = storelist.concat(list);
-            pageobject.setData({
-              storearray: storelist,
-            })
-          } else {
+      url: durl + "/store/searchfood",
+      header: { Cookie: "JSESSIONID=" + app.globalData.session },
+      data: data,
+      success: function (res) {
+        if (res.data.status == 200) {
+          console.log("相关商店", res.data.data)
+          var list = res.data.data;
+          wx.hideLoading();
+          if (list.length == 0) {
             wx.showToast({
-              icon: 'none',
-              title: '加载失败',
+              icon: "none",
+              title: "没有更多数据了",
             })
+            return;
           }
-        },
-        fail: function () {
+          for (var i = 0; i < list.length; i++) {
+            list[i].type = list[i].type.split(",");
+            list[i].imgsrc = durl + "/static/image/" + list[i].id + "image.jpg";
+            for (var z = 0; z < list[i].foodlist.length; z++)
+              list[i].foodlist[z].foodimg = durl + "/static/image/food/food" + list[i].foodlist[z].foodid + ".jpg";
+          }
+          storelist = storelist.concat(list);
+          pageobject.setData({
+            storearray: storelist,
+          })
+        } else {
           wx.showToast({
             icon: 'none',
             title: '加载失败',
           })
         }
-      })
-    }
-
-    function change() {
-      //检查用户是否授予定位权限
-      positionpromise()
-        .catch(function (res) {
-          //向用户获取位置权限
-          return getlocaltionauthorizepromise();
+      },
+      fail: function () {
+        wx.showToast({
+          icon: 'none',
+          title: '加载失败',
         })
-        .then(function (res) {
-          if (latitude == null || longitude == null || (latitude == 0 && longitude == 0)) {
-            //获取用户位置信息（经纬度信息）
-            return getlocationpromise();
-          }
-        })
-        .then(function (res) {
-          getstorespromise(res)
-        })
-    }
+      }
+    })
+  },
 
-    pageobject.change = change;
-
-    change();
+  onLoad: function (options) {
+    storelist = [];
+    currentpage = 1;
+    pageobject = this;
+    keyword = options.keyword;
+    pageobject.getstores();
 
   },
   /**
@@ -191,7 +104,7 @@ Page({
       mask: true
     })
     currentpage++;
-    this.change();
+    this.getstores();
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
